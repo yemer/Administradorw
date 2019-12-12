c7f3115d435bc85d657d819766591561b90fe1e7
yemer <i1610795@continental.edu.pe>
2019-12-12 03:21:02 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { ContentComponent } from './components/shared/content/content.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SettingComponent } from './components/shared/setting/setting.component';
+import { PanelDecontrolComponent } from './pages/panel-decontrol/panel-decontrol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent,
+    SettingComponent,
+    PanelDecontrolComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/panel-decontrol/panel-decontrol.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-decontrol',
+  templateUrl: './panel-decontrol.component.html',
+  styleUrls: ['./panel-decontrol.component.css']
+})
+export class PanelDecontrolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
